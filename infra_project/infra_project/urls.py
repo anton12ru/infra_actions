0fe6b1a6db7edaba455b0a14b9266b9c26615134
Anton <kampris2013@yandex.ru>
2022-06-14 21:52:24 +0300

    fixed by PEP8

--- infra_project/infra_project/urls.py
+++ infra_project/infra_project/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path, include
 from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
-    path('', include('infra_app.urls', namespace='infra_app')),
-    path('admin/', admin.site.urls),
+    path("", include("infra_app.urls", namespace="infra_app")),
+    path("admin/", admin.site.urls),
 ]
